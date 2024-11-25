0a116790ae8cafad836a6102a985d822c6bc8ca9
dubail73 <sdubail2000@gmail.com>
2024-11-25 20:49:33 +0100

    cart

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,15 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable
+
+  has_one :cart, dependent: :destroy
+
+  after_create :create_cart
+
+  private
+
+  def create_cart
+    Cart.create(user: self)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :items
+  resource :cart, only: [:show] do
+    delete 'remove_item/:id', to: 'carts#remove_item', as: 'remove_item'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
