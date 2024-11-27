c068bc073106e7ed1fc68007b8fe84f45b54293f
dubail73 <sdubail2000@gmail.com>
2024-11-27 08:21:37 +0100

    stripe/seed

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,10 @@ Rails.application.routes.draw do
   resource :cart, only: [:show] do
     post 'add_item/:item_id', to: 'carts#add_item', as: 'add_item'
     delete 'remove_item/:id', to: 'carts#remove_item', as: 'remove_item'
+   
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :galeries, only: [:index]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
