5bb03168bf4f0f35a225681fda06554a782b9ba9
dubail73 <sdubail2000@gmail.com>
2024-11-27 12:01:05 +0100

    stripe/boutton paiement

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
  
   devise_for :users
-  resources :orders, only: [:new, :create]
+  #resources :orders, only: [:new, :create]
   resources :items
   resource :cart, only: [:show] do
     post 'add_item/:item_id', to: 'carts#add_item', as: 'add_item'
@@ -22,4 +22,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "items#index"
+  scope '/checkout' do
+    post 'create', to: 'checkout#create', as: 'checkout_create'
+    get 'success', to: 'checkout#success', as: 'checkout_success'
+    get 'cancel', to: 'checkout#cancel', as: 'checkout_cancel'
+end
+
 end
